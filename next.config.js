309c5aa86c399acd4c64249e8316afb5f7b05b75
k6chan <k6chan@ucsd.edu>
2021-11-18 23:44:10 -0800

    remove isProd, set env variable GITHUB_ACTIONS to true

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/animefest-next-site/' : ''
+  assetPrefix: isGithubActions ? '/animefest-next-site/' : ''
 }
