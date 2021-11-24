2a0cb5170385131e0be83b9bc62299f187cda20b
k6chan <k6chan@ucsd.edu>
2021-11-23 21:56:56 -0800

    change basepath for local hosting from '/' to ''

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
 module.exports = {
   reactStrictMode: true,
   assetPrefix: isGithubActions ? '/animefest-next-site/' : '',
-  basePath: isGithubActions ? '/animefest-next-site' : '/'
+  basePath: isGithubActions ? '/animefest-next-site' : ''
 }
