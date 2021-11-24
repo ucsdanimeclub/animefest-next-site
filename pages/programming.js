14aab8cd7d20cf8f80ea1998a91edf83cb942a8b
k6chan <k6chan@ucsd.edu>
2021-11-23 22:16:22 -0800

    move all css from Home.modules.css to globals, remove Home.modules.css import

--- pages/programming.js
+++ pages/programming.js
@@ -1,41 +1,40 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Button from 'react-bootstrap/Button'
 
 
 export default function Programming() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Animefest 2022</title>
         <meta name="description" content="February 12 at UC San Diego's Price Center" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="main">
+        <h1 className="title">
           Programming
         </h1>
 
-        <p className={styles.description}>
+        <p className="description">
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className="code">pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div className="grid">
+          <a href="https://nextjs.org/docs" className="card">
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn" className="card">
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            className="card"
           >
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
@@ -43,7 +42,7 @@ export default function Programming() {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="card"
           >
             <h2>Deploy &rarr;</h2>
             <p>
@@ -55,14 +54,14 @@ export default function Programming() {
         <Button variant="outline-primary">Vote for the winners here!</Button>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="footer">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span className="logo">
             <img src="images/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
