23a9410553941a5ee9f34e51cedab5f7d4083d21
k6chan <k6chan@ucsd.edu>
2022-01-16 23:30:31 -0600

    add headers to navbar

--- components/layout.js
+++ components/layout.js
@@ -10,27 +10,29 @@ export default function Layout({ children }) {
       <Navbar bg="light" expand="lg" sticky="top">
         <Container>
           <Link href="/" passHref><Navbar.Brand className="display-text">ANIMEFEST</Navbar.Brand></Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
+            <Nav navbarScroll className="ms-auto">
               <NavDropdown title="Home" id="basic-nav-dropdown">
                 <Link href='/' passHref><NavDropdown.Item>Home</NavDropdown.Item></Link>
                 <NavDropdown.Divider />
                 <Link href='/#about' passHref><NavDropdown.Item>About</NavDropdown.Item></Link>
                 <Link href='/#map' passHref><NavDropdown.Item>Map and Schedule</NavDropdown.Item></Link>
                 <Link href='/#parking' passHref><NavDropdown.Item>Parking</NavDropdown.Item></Link>
-                {/* <Link href='/#sponsors' passHref><NavDropdown.Item>Photobooth</NavDropdown.Item></Link> */}
+                {/* <Link href='/#photobooth' passHref><NavDropdown.Item>Photobooth</NavDropdown.Item></Link> */}
               </NavDropdown>
               <NavDropdown title="Programming" id="basic-nav-dropdown">
-                <Link href='/programming' passHref><NavDropdown.Item>Programming</NavDropdown.Item></Link>
-                <NavDropdown.Divider />
+                <NavDropdown.Header>West Ballroom</NavDropdown.Header>
                 <Link href="/programming#main" passHref><NavDropdown.Item>Main Stage</NavDropdown.Item></Link>
                 <Link href="/programming#exhibitor" passHref><NavDropdown.Item>Exhibitor Hall</NavDropdown.Item></Link>
-                <Link href="/programming#panels" passHref><NavDropdown.Item>Panels</NavDropdown.Item></Link>
+                <NavDropdown.Header>Price Center Theater</NavDropdown.Header>
                 <Link href="/programming#performances" passHref><NavDropdown.Item>Performances and Showings</NavDropdown.Item></Link>
+                <NavDropdown.Header>College Rooms</NavDropdown.Header>
+                <Link href="/programming#panels" passHref><NavDropdown.Item>Panels</NavDropdown.Item></Link>
+                <Link href="/programming#manga" passHref><NavDropdown.Item>Manga Library</NavDropdown.Item></Link>
+                <NavDropdown.Header>Sun God Lounge</NavDropdown.Header>
                 <Link href="/programming#gaming" passHref><NavDropdown.Item>Gaming</NavDropdown.Item></Link>
                 <Link href="/programming#tabletop" passHref><NavDropdown.Item>Tabletop Games and Origami</NavDropdown.Item></Link>
-                <Link href="/programming#manga" passHref><NavDropdown.Item>Manga Library</NavDropdown.Item></Link>
               </NavDropdown>
               <Link href='/food' passHref><Nav.Link>Food</Nav.Link></Link>
               <Link href='/rules' passHref><Nav.Link>Rules</Nav.Link></Link>
