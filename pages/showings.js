0946e81db57842d47c2221e07dceeabc110057e9
k6chan <k6chan@ucsd.edu>
2022-01-17 20:59:46 -0600

    remove showings signup and add breakpoint for the text column

--- pages/showings.js
+++ pages/showings.js
@@ -11,10 +11,9 @@ export default function Showings() {
 
       <Container fluid className="p-5 section">
         <Row className="align-items-start">
-          <Col>
+          <Col md={8}>
             <h4 className="text-left mb-4 display-text">ABOUT</h4>
             <p>Performances will be hosted at the Price Center Theater, which is located on the first floor and below the Bear Room.</p>
-            <p>If you are a performer and are interested in performing at Animefest, please view our Performers application <a href="https://forms.gle/U5vXp6oN8YotrfzBA">here.</a> The deadline to apply is January 22nd.</p>
             <p>For this year&apos;s animated film, we will be showcasing famous Japanese film director Shinkai Makoto&apos;s &quot;Weathering With You&quot;, or Tenki no Ko.
               Following its predecessor, Kimi no Nawa, Tenki no Ko, is a modern romance film excelling in audiovisuals, emotional drama, and a picturesque perspective on climate change in Tokyo.
               After selling millions of tickets in 2019, the film won Animaton of the Year award at the 43rd annual Japan Academy Prize Ceremony as well as the Social Impact Award at the 23rd Japan Media Arts Festival.
