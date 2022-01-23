4ae55bf30d36705bb73190e458a93e898a43c18d
k6chan <k6chan@ucsd.edu>
2022-01-23 12:24:24 -0600

    table assignments delay text

--- pages/exhibitor.js
+++ pages/exhibitor.js
@@ -11,7 +11,7 @@ export default function Exhibitor() {
       <Container fluid className="p-5 section">
         <h4 className="text-left mb-4 display-text">ABOUT</h4>
         <p>The Exhibitor Hall is home to our Artist Alley and Vendor space. It is located throughout the West Ballroom.</p>
-        <p className="mb-0">Artist Alley and Vendor assignments will be made public by January 23rd.</p>
+        <p className="mb-0">Please wait for an update on Artist Alley and Vendor table assignments. We apologize for the delay!</p>
       </Container>
     </>
   )
