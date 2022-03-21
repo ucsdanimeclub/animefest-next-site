d38be45ba90b8c91d9904fa97b92544ec85435af
k6chan <k6chan@ucsd.edu>
2022-03-21 02:57:16 -0700

    add hidden code to call exhibitor table component

--- pages/exhibitor.js
+++ pages/exhibitor.js
@@ -1,18 +1,23 @@
-import { Container } from 'react-bootstrap'
+import {Container} from 'react-bootstrap'
+// import Artists from "../data/artists.json"
+// import Vendors from "../data/vendors.json"
+// import ExhibitorTable from "../components/exhibitor/exhibitortable"
 
 
 export default function Exhibitor() {
-  return (
-    <>
-      <Container fluid className="p-5 text-center jumbo-display">
-        <h1 className="display-4 mb-0">EXHIBITOR HALL</h1>
-      </Container>
+    return (
+        <>
+            <Container fluid className="p-5 text-center jumbo-display">
+                <h1 className="display-4 mb-0">EXHIBITOR HALL</h1>
+            </Container>
 
-      <Container fluid className="p-5 section">
-        <h4 className="text-left mb-4 display-text">ABOUT</h4>
-        <p>The Exhibitor Hall is home to our Artist Alley and Vendor space. It is located throughout the West Ballroom.</p>
-        <p className="mb-0">Table assignments coming soon!</p>
-      </Container>
-    </>
-  )
+            <Container fluid className="p-5 section">
+                <h4 className="text-left mb-4 display-text">ABOUT</h4>
+                <p>The Exhibitor Hall is home to our Artist Alley and Vendor space. It is located throughout the West
+                    Ballroom.</p>
+                <p className="mb-0">Table assignments coming soon!</p>
+                {/*<ExhibitorTable data={Artists}/>*/}
+            </Container>
+        </>
+    )
 }
