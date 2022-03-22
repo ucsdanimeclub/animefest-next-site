315539c4053bc3b845376f54caad3daa6262baaa
k6chan <k6chan@ucsd.edu>
2022-03-22 01:23:55 -0700

    add support for other props and centering

--- components/index/social.js
+++ components/index/social.js
@@ -3,10 +3,20 @@ import Socials from "../../data/socials.json"
 import MyImage from "../myimage"
 
 
-export default function Social({ children, height = 50 }) {
-  return (
-    <Container fluid className="p-4 text-center">
-      <ListGroup horizontal className="justify-content-center mb-2">
+export default function Social({ children, height = 50, centered = true, ...otherProps }) {
+    let {className, ...otherPropsWithoutClassName} = otherProps
+    let classes = "p-4"
+    let listClasses = "mb-2"
+    if (centered) {
+        classes = classes + " text-center"
+        listClasses = listClasses + " justify-content-center"
+    }
+    if (className) {
+        classes = classes + " " + className
+    }
+    return (
+    <Container fluid className={classes} {...otherPropsWithoutClassName}>
+      <ListGroup horizontal className={listClasses}>
         {Socials['socials'].map((social, index) => (
           <ListGroup.Item className="bg-transparent" key={index}>
             {
--- components/navigation/sponsor.js
+++ components/navigation/sponsor.js
@@ -3,9 +3,17 @@ import Sponsors from "../../data/sponsors.json"
 import MyImage from "../myimage"
 
 
-export default function Sponsor() {
+export default function Sponsor({centered = true,...otherProps}) {
+  let {className, ...otherPropsWithoutClassName} = otherProps
+  let classes = "mx-4 mb-4 gy-4"
+  if (centered) {
+    classes = classes + " justify-content-center"
+  }
+  if (className) {
+    classes = classes + " " + className
+  }
   return (
-    <Row className="mx-4 mb-4 gy-4 justify-content-center">
+    <Row className={classes} {...otherPropsWithoutClassName}>
       {Sponsors['sponsors'].map((sponsor, index) => (
         <Col xs={12} sm={6} md={3} key={index}>
           <a href={sponsor['url']}>
