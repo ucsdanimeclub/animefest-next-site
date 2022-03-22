1e3d84bf7d1c25499058d2eba952b77b2eab6923
k6chan <k6chan@ucsd.edu>
2022-03-22 00:48:57 -0700

    allow other props to be added

--- components/index/programscarousel.js
+++ components/index/programscarousel.js
@@ -3,13 +3,17 @@ import Programs from "../../data/programs.json"
 import MyImage from "../myimage"
 
 
-export default function ProgramsCarousel({centered = true}) {
+export default function ProgramsCarousel({centered = true, ...otherProps}) {
+  let {className, ...otherPropsWithoutClassName} = otherProps
   let classes = "carousel border-stroke caption-text glow-text"
   if (centered) {
     classes = classes + " mx-auto"
   }
+  if (className) {
+    classes = classes + " " + className
+  }
   return (
-    <Carousel className={classes}>
+    <Carousel className={classes} {...otherPropsWithoutClassName}>
       {Programs['programs'].map((program, index) => (
         <Carousel.Item key={index}>
           <MyImage
