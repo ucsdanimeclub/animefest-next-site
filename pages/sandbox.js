ab8b10e58f59897fa3b5d81eecd1a1852d0dca17
k6chan <k6chan@ucsd.edu>
2022-03-22 00:58:09 -0700

    separate artists and vendors tables

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -8,7 +8,9 @@ export default function Sandbox() {
     return (
         <>
             <Container fluid className="p-5 section">
+                <h4 className="text-left title-text mb-4">Artists</h4>
                 <ExhibitorTable data={Artists} className="mb-4"/>
+                <h4 className="text-left title-text mb-4">Vendors</h4>
                 <ExhibitorTable data={Vendors}/>
             </Container>
         </>
