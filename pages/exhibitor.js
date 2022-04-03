5d635625b2845e688654d35ff4665baa1e421045
k6chan <k6chan@ucsd.edu>
2022-04-03 01:08:14 -0700

    update exhibitor release date

--- pages/exhibitor.js
+++ pages/exhibitor.js
@@ -15,7 +15,7 @@ export default function Exhibitor() {
                 <h4 className="text-left mb-4 display-text">ABOUT</h4>
                 <p>The Exhibitor Hall is home to our Artist Alley and Vendor space. It is located throughout the West
                     Ballroom.</p>
-                <p className="mb-0">Table assignments coming soon!</p>
+                <p className="mb-0">Table assignments will be released one month before the convention date.</p>
                 {/*<ExhibitorTable data={Artists}/>*/}
             </Container>
         </>
