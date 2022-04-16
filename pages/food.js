4b64209ebd75fa3a115f71a5a624f2edbafcd33b
k6chan <k6chan@ucsd.edu>
2022-04-16 10:54:06 -0700

    complete food page with coupon

--- pages/food.js
+++ pages/food.js
@@ -1,17 +1,24 @@
-import { Container } from 'react-bootstrap'
+import {Container} from 'react-bootstrap'
+import MyImage from "../components/myimage";
 
 
 export default function Food() {
-  return (
-    <>
-      <Container fluid className="p-5 text-center jumbo-display">
-        <h1 className="display-4 mb-0">FOOD</h1>
-      </Container>
+    return (
+        <>
+            <Container fluid className="p-5 text-center jumbo-display">
+                <h1 className="display-4 mb-0">FOOD</h1>
+            </Container>
 
-      <Container fluid className="p-5 section">
-        {/* <h4 className="text-left title-text mb-4">Dining at Price Center</h4> */}
-        Coming Soon!
-      </Container>
-    </>
-  )
+            <Container fluid className="p-5 section">
+                <h4 className="text-left mb-4 display-text">ABOUT</h4>
+                <p className="mb-4">Animefest is located in UCSD&apos;s Price Center, which has a variety of <a
+                    href="https://universitycenters.ucsd.edu/dining-retail/dining/">dining
+                    options</a> convenient for staying fed during the convention.
+                </p>
+                <a href="images/food_coupon.png"><MyImage src="images/food_coupon.png" alt="Food coupon"
+                                                          className="mw-100 border-stroke mx-auto d-block mb-1"/></a>
+                <p className="text-center caption-text">Click on the image to enlarge.</p>
+            </Container>
+        </>
+    )
 }
