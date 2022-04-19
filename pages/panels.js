bfec57422972a694ed5b70d6e1e892a1292f40f2
k6chan <k6chan@ucsd.edu>
2022-04-19 13:11:58 -0700

    remove panel signups

--- pages/panels.js
+++ pages/panels.js
@@ -11,9 +11,8 @@ export default function Panels() {
       <Container fluid className="p-5 section">
         <h4 className="text-left mb-4 display-text">ABOUT</h4>
         <p>Panels will be held at the Thurgood Marshall and Eleanor Roosevelt rooms in Price Center.</p>
-        <p>The best place for learning more not just about anime, but passion and culture as well! Interesting discussion topics will be present and available for participation at your discretion!
+        <p className="mb-0">The best place for learning more not just about anime, but passion and culture as well! Interesting discussion topics will be present and available for participation at your discretion!
           Some panels will even have workshops for a hands-on experience. More panels and information coming soon!</p>
-        <p className="mb-0">Interested in hosting a panel? Please view our Panelist application <a href="https://forms.gle/FmQKZ6JMe9xvr3CU8">here.</a> The deadline to apply has been extended to April 16.</p>
       </Container>
     </>
   )
