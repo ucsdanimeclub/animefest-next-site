77900f5f66d74df01ad5e455317959bb81fcfe91
k6chan <k6chan@ucsd.edu>
2022-04-29 14:56:05 -0700

    add disclaimer

--- components/prizes/prizeimages.js
+++ components/prizes/prizeimages.js
@@ -1,21 +1,24 @@
 import MyImage from "../myimage"
 
 
-export default function PrizeImages({ prizes, ...otherProps }) {
-    let { className, ...otherPropsWithoutClassName } = otherProps
-    let classes = "d-inline-flex flex-wrap justify-content-between"
+export default function PrizeImages({prizes, ...otherProps}) {
+    let {className, ...otherPropsWithoutClassName} = otherProps
+    let classes = "mb-2 d-inline-flex flex-wrap justify-content-between"
     if (className) {
         classes = classes + " " + className
     }
     return (
-        <div className={classes} {...otherPropsWithoutClassName}>
-            {prizes.map((imageSource, index) => {
-                return (
-                    <MyImage key={index} src={imageSource}
-                        className="col-xs-6 col-sm-4 col-md-3 col-lg-2 m-2 border-stroke prize-image">
-                    </MyImage>
-                )
-            })}
-        </div>
+        <>
+            <div className={classes} {...otherPropsWithoutClassName}>
+                {prizes.map((imageSource, index) => {
+                    return (
+                        <MyImage key={index} src={imageSource}
+                                 className="col-xs-6 col-sm-4 col-md-3 col-lg-2 m-2 border-stroke prize-image">
+                        </MyImage>
+                    )
+                })}
+            </div>
+            <p className="text-center caption-text">Actual designs may vary.</p>
+        </>
     )
 }
