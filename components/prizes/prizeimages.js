a27b468b7412de4ac17ab297bc04e5f191c93bd0
k6chan <k6chan@ucsd.edu>
2022-04-30 00:34:55 -0700

    add links to images

--- components/prizes/prizeimages.js
+++ components/prizes/prizeimages.js
@@ -1,8 +1,8 @@
 import MyImage from "../myimage"
 
 
-export default function PrizeImages({prizes, ...otherProps}) {
-    let {className, ...otherPropsWithoutClassName} = otherProps
+export default function PrizeImages({ prizes, ...otherProps }) {
+    let { className, ...otherPropsWithoutClassName } = otherProps
     let classes = "mb-2 d-inline-flex flex-wrap justify-content-between"
     if (className) {
         classes = classes + " " + className
@@ -12,9 +12,11 @@ export default function PrizeImages({prizes, ...otherProps}) {
             <div className={classes} {...otherPropsWithoutClassName}>
                 {prizes.map((imageSource, index) => {
                     return (
-                        <MyImage key={index} src={imageSource}
-                                 className="col-xs-6 col-sm-4 col-md-3 col-lg-2 m-2 border-stroke prize-image">
-                        </MyImage>
+                        <a key={index} href={imageSource} style={{ "display": "contents" }}>
+                            <MyImage src={imageSource}
+                                className="col-xs-6 col-sm-4 col-md-3 col-lg-2 m-2 border-stroke prize-image">
+                            </MyImage>
+                        </a>
                     )
                 })}
             </div>
