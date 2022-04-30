f668d75616fd6fb4298525fc62f296e40ddfa3a7
k6chan <k6chan@ucsd.edu>
2022-04-29 18:49:24 -0700

    change style structure of schedule

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -48,7 +48,7 @@ export default function Sandbox() {
 
             <Container fluid className="p-4 mt-5 section">
                 <div>
-                    <iframe className="sheet-schedule w-75 d-block mx-auto"
+                    <iframe className="sheet-schedule sheet-constraint w-75 d-block mx-auto"
                             src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSh2gBYXSEV57p05RAVENMwqDHmJ5GFHTjgb0M9LOTAYrBbLrrwA5fMECz7R3F2qfpnRElr854rGZOG/pubhtml?widget=true&amp;headers=false"/>
                 </div>
             </Container>
