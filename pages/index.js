f0e16cc1df1b9307dd42ee6c0f2cca7132c4404a
k6chan <k6chan@ucsd.edu>
2022-04-29 22:56:57 -0700

    display schedule and map

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {Col, Container, Ratio, Row} from 'react-bootstrap'
+import { Col, Container, Ratio, Row } from 'react-bootstrap'
 import BackgroundImage from "../components/index/backgroundimage"
 import ProgramsCarousel from "../components/programscarousel"
 import Social from "../components/index/social"
@@ -19,7 +19,7 @@ export default function Home() {
                 <BackgroundImage src="images/landing.jpg" className="landing">
                     <Row className="align-items-center h-75 g-0 glow-text">
                         <Col>
-                            <MyImage src="images/logo_af.svg" className="w-75" alt="Animefest 2022 logo"/>
+                            <MyImage src="images/logo_af.svg" className="w-75" alt="Animefest 2022 logo" />
                             <h4>May 7 at UC San Diego&apos;s Price Center</h4>
                             <h4>11:00 AM to 8:00 PM</h4>
                             <h5>Free Admission</h5>
@@ -43,37 +43,36 @@ export default function Home() {
                 <div className="video-player mx-auto mb-5">
                     <Ratio aspectRatio="16x9">
                         <iframe className="border-stroke"
-                                src="https://www.youtube-nocookie.com/embed/eITsJPrRF4c"
-                                title="YouTube video player" frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen={true}/>
+                            src="https://www.youtube-nocookie.com/embed/eITsJPrRF4c"
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen={true} />
                     </Ratio>
                 </div>
-                <ProgramsCarousel data={Programs["programs"]}/>
+                <ProgramsCarousel data={Programs["programs"]} />
             </Container>
 
             <Container fluid className="p-5 mt-4 section" id="map">
                 <h1 className="text-left mb-4 display-text">MAP AND SCHEDULE</h1>
-                <p>Animefest is held at the Price Center at UC San Diego. Specifically, it will be at the West Ballroom,
-                    Theater, Thurgood Marshall, Eleanor Roosevelt, Roger Revelle, Red Shoe, Bear, and Green Table
+                <p>Animefest is held at the Price Center at UC San Diego. Specifically, it will be at the West Ballroom, Thurgood Marshall,
+                    Eleanor Roosevelt, Roger Revelle, Theater, Red Shoe, Bear, and Green Table
                     rooms.</p>
-                <p>All rooms are located on the second floor with the exception of the Theater, which is on the first
+                <p className="mb-4">All rooms are located on the second floor with the exception of the Theater, which is on the first
                     floor and below the Bear Room.</p>
-                <p className="mb-0">Full map and schedule coming soon!</p>
-                {/*<div className="text-center mb-5">*/}
-                {/*    <iframe className="sheet-schedule sheet-constraint w-75 d-block mb-1 mx-auto"*/}
-                {/*            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSh2gBYXSEV57p05RAVENMwqDHmJ5GFHTjgb0M9LOTAYrBbLrrwA5fMECz7R3F2qfpnRElr854rGZOG/pubhtml?widget=true&amp;headers=false"/>*/}
-                {/*    <p className="caption-text"><a*/}
-                {/*        href="https://docs.google.com/spreadsheets/d/11egWL19jhBBIf27SXAjf81tyDxkVJsRjCtNo4gr5bTE/edit?usp=sharing">Have*/}
-                {/*        trouble viewing? Click here!</a></p>*/}
-                {/*</div>*/}
-                {/*<div className="text-center">*/}
-                {/*    <a href="images/map.jpg">*/}
-                {/*        <MyImage src="images/map.jpg" className="sheet-constraint border-stroke mw-100 mb-1"*/}
-                {/*                 alt="Price Center map"></MyImage>*/}
-                {/*    </a>*/}
-                {/*    <p className="caption-text mb-0">Click on the image to open.</p>*/}
-                {/*</div>*/}
+                <div className="text-center mb-5">
+                    <iframe className="sheet-schedule sheet-constraint w-75 d-block mb-1 mx-auto"
+                        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSh2gBYXSEV57p05RAVENMwqDHmJ5GFHTjgb0M9LOTAYrBbLrrwA5fMECz7R3F2qfpnRElr854rGZOG/pubhtml?widget=true&amp;headers=false" />
+                    <p className="caption-text"><a
+                        href="https://docs.google.com/spreadsheets/d/11egWL19jhBBIf27SXAjf81tyDxkVJsRjCtNo4gr5bTE/edit?usp=sharing">Have
+                        trouble viewing? Click here!</a></p>
+                </div>
+                <div className="text-center">
+                    <a href="images/map.jpg">
+                        <MyImage src="images/map.jpg" className="sheet-constraint border-stroke mw-100 mb-1"
+                            alt="Price Center map"></MyImage>
+                    </a>
+                    <p className="caption-text mb-0">Click on the image to open.</p>
+                </div>
             </Container>
 
             <Container fluid className="p-5 mt-4 section" id="parking">
@@ -93,8 +92,8 @@ export default function Home() {
                 <div className="map mx-auto">
                     <Ratio aspectRatio="4x3">
                         <iframe className="border-stroke"
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.697394192468!2d-117.2383838856308!3d32.87972498605018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06c46a524fb9%3A0x68571efb243bc289!2sPrice%20Center%2C%20La%20Jolla%2C%20CA%2092093!5e0!3m2!1sen!2sus!4v1638064542514!5m2!1sen!2sus"
-                                width="600" height="450" allowFullScreen="" loading="lazy"/>
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.697394192468!2d-117.2383838856308!3d32.87972498605018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06c46a524fb9%3A0x68571efb243bc289!2sPrice%20Center%2C%20La%20Jolla%2C%20CA%2092093!5e0!3m2!1sen!2sus!4v1638064542514!5m2!1sen!2sus"
+                            width="600" height="450" allowFullScreen="" loading="lazy" />
                     </Ratio>
                 </div>
             </Container>
