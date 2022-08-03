a1e1ce2a64d7919b4b024da0d3bf2de2302fa91d
Kaitlyn <k6chan@ucsd.edu>
2022-08-02 20:58:35 -0500

    add font weight variety

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,24 @@
-import { useRouter } from "next/dist/client/router"
+import {useRouter} from "next/dist/client/router"
 import Head from 'next/head'
 import Layout from '../components/navigation/layout'
 import '../styles/globals.css'
 import '../styles/theme.scss'
 
 
-export default function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  return (
-    <>
-      <Head>
-        <title>Animefest 2022</title>
-        <meta name="description" content="May 7, 2022 at UC San Diego's Price Center" />
-        <link rel="icon" href={`${router.basePath}/favicon.ico`}/>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+export default function MyApp({Component, pageProps}) {
+    const router = useRouter()
+    return (
+        <>
+            <Head>
+                <title>Animefest 2022</title>
+                <meta name="description" content="May 7, 2022 at UC San Diego's Price Center"/>
+                <link rel="icon" href={`${router.basePath}/favicon.ico`}/>
+                <link rel="stylesheet" href={`${router.basePath}/fonts.css`}/>
+            </Head>
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
-  )
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    )
 }
