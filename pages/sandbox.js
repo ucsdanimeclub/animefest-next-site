df67e1ba8cd5bd8800e51de2cc21c5fd3bfc5366
Kaitlyn <k6chan@ucsd.edu>
2022-11-11 15:20:32 -0800

    clear sandbox

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -1,26 +1,6 @@
-import { Container } from 'react-bootstrap'
-import PrizeDisplay from "../components/prizes/prizedisplay"
-import Prizes from "../data/prizes.json"
-
-
 export default function Sandbox() {
-    const prizePools = [
-        "Afternoon Raffle", "Cosplay Masquerade", "Evening Raffle", "Fist of the North Star",
-        "Genshin Cosplay", "Kancolle", "Modelkit", "Smash Ultimate"
-    ]
-    const prizesInPool = Prizes["prizes"].filter((prize) => prize["pool"] === "Afternoon Raffle").map((prize) => prize["image"])
     return (
         <>
-            <Container fluid className="p-5 section">
-                <PrizeDisplay data={Prizes["prizes"]} pools={prizePools} />
-            </Container>
-
-            <Container fluid className="p-4 mt-5 section">
-                <div>
-                    <iframe className="sheet-schedule sheet-constraint w-75 d-block mx-auto"
-                        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSh2gBYXSEV57p05RAVENMwqDHmJ5GFHTjgb0M9LOTAYrBbLrrwA5fMECz7R3F2qfpnRElr854rGZOG/pubhtml?widget=true&amp;headers=false" />
-                </div>
-            </Container>
         </>
     )
 }
