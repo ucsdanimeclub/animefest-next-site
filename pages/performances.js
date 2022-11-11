59bb17c569d97fbb6000af481911483f3095b639
Kaitlyn <k6chan@ucsd.edu>
2022-11-11 15:07:15 -0800

    update theater page with placeholder info

--- pages/performances.js
+++ pages/performances.js
@@ -10,19 +10,25 @@ export default function Performances() {
 
             <Container fluid className="p-3 section">
                 <h4 className="text-left mb-4 display-text">ABOUT</h4>
-                {/*<p className="text-left caption-text mb-4">3:00 PM - 4:00 PM: Performance</p>*/}
                 <p>Performances will be held in the East Ballroom. More information coming soon!</p>
-                {/*<p className="mb-4">Sample:</p>*/}
-                {/*<div className="video-player mx-auto">*/}
-                {/*    <Ratio aspectRatio="16x9">*/}
-                {/*        <iframe className="border-stroke"*/}
-                {/*                src="https://www.youtube-nocookie.com/embed/dXodViGW8c8"*/}
-                {/*                title="YouTube video player"*/}
-                {/*                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"*/}
-                {/*                allowFullScreen={true} />*/}
-                {/*    </Ratio>*/}
-                {/*</div>*/}
             </Container>
+
+            {/*<Container fluid className="p-3 mt-4 section">*/}
+            {/*    <h4 className="text-left mb-4 display-text">FEATURING SUPER TONIC</h4>*/}
+            {/*    <p className="text-left caption-text mb-4">3:00 PM - 4:00 PM: Performance</p>*/}
+            {/*    <p className="mb-4">A San Diego based anime and video game cover band composed of UCSD alumni, with a*/}
+            {/*        set of songs sure to deliver a huge dose of excitement. Check out their last Animefest performance*/}
+            {/*        here:</p>*/}
+            {/*    <div className="video-player mx-auto">*/}
+            {/*        <Ratio aspectRatio="16x9">*/}
+            {/*            <iframe className="border-stroke"*/}
+            {/*                    src="https://www.youtube-nocookie.com/embed/Ul4fmEYF7Bk"*/}
+            {/*                    title="YouTube video player" frameBorder="0"*/}
+            {/*                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"*/}
+            {/*                    allowFullScreen={true}/>*/}
+            {/*        </Ratio>*/}
+            {/*    </div>*/}
+            {/*</Container>*/}
         </>
     )
 }
--- pages/theater.js
+++ pages/theater.js
@@ -1,60 +1,43 @@
-import {Col, Container, Ratio, Row} from 'react-bootstrap'
-import MyImage from "../components/myimage"
+import {Container} from 'react-bootstrap' // Row and Col for the poster image
+// import MyImage from "../components/myimage"
 
 
 export default function Theater() {
     return (
         <>
             <Container fluid className="p-5 text-center jumbo-display">
-                <h1 className="display-4 mb-0">THEATER</h1>
+                <h1 className="display-4 mb-0">MOVIE SHOWING</h1>
             </Container>
 
-            <Container fluid className="p-5 section">
+            <Container fluid className="p-3 section">
                 <h4 className="text-left mb-4 display-text">ABOUT</h4>
                 <p>The Price Center Theater is located on the
                     first floor and
-                    below the Bear Room. This year, look forward to a musical performance by Super Tonic!</p>
-                <p className="mb-0">We will also be hosting two showings of Weathering With You throughout the day.</p>
+                    below the Bear Room.</p>
+                <p>We will be hosting two showings of a movie throughout the day. Check back here closer to the event date for more information!</p>
             </Container>
 
-            <Container fluid className="p-5 mt-4 section">
-                <h4 className="text-left mb-4 display-text">FEATURING SUPER TONIC</h4>
-                <p className="text-left caption-text mb-4">3:00 PM - 4:00 PM: Performance</p>
-                <p className="mb-4">A San Diego based anime and video game cover band composed of UCSD alumni, with a
-                    set of songs sure to deliver a huge dose of excitement. Check out their last Animefest performance
-                    here:</p>
-                <div className="video-player mx-auto">
-                    <Ratio aspectRatio="16x9">
-                        <iframe className="border-stroke"
-                                src="https://www.youtube-nocookie.com/embed/Ul4fmEYF7Bk"
-                                title="YouTube video player" frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen={true}/>
-                    </Ratio>
-                </div>
-            </Container>
-
-            <Container fluid className="p-5 mt-4 section">
-                <Row className="align-items-start">
-                    <Col md={8}>
-                        <h4 className="text-left mb-4 display-text">WEATHERING WITH YOU</h4>
-                        <p className="text-left caption-text mb-1">11:30 AM - 1:30 PM: First Showing</p>
-                        <p className="text-left caption-text mb-4">5:00 PM - 7:00 PM: Second Showing</p>
-                        <p>For this year&apos;s animated film, we will be showcasing famous Japanese film director
-                            Shinkai Makoto&apos;s &quot;Weathering With You&quot;, or Tenki no Ko.
-                            Following its predecessor, Kimi no Nawa, Tenki no Ko, is a modern romance film excelling in
-                            audiovisuals, emotional drama, and a picturesque perspective on climate change in Tokyo.
-                            After selling millions of tickets in 2019, the film won Animaton of the Year award at the
-                            43rd annual Japan Academy Prize Ceremony as well as the Social Impact Award at the 23rd
-                            Japan Media Arts Festival.
-                            In other words, it&apos;s a must-see!</p>
-                    </Col>
-                    <Col md={4}>
-                        <MyImage src="images/wwy_poster.jpg" alt="Weathering With You poster"
-                                 className="border-stroke mw-100 mx-auto d-block"/>
-                    </Col>
-                </Row>
-            </Container>
+            {/*<Container fluid className="p-3 mt-4 section">*/}
+            {/*    <Row className="align-items-start">*/}
+            {/*        <Col md={8}>*/}
+            {/*            <h4 className="text-left mb-4 display-text">WEATHERING WITH YOU</h4>*/}
+            {/*            <p className="text-left caption-text mb-1">11:30 AM - 1:30 PM: First Showing</p>*/}
+            {/*            <p className="text-left caption-text mb-4">5:00 PM - 7:00 PM: Second Showing</p>*/}
+            {/*            <p>For this year&apos;s animated film, we will be showcasing famous Japanese film director*/}
+            {/*                Shinkai Makoto&apos;s &quot;Weathering With You&quot;, or Tenki no Ko.*/}
+            {/*                Following its predecessor, Kimi no Nawa, Tenki no Ko, is a modern romance film excelling in*/}
+            {/*                audiovisuals, emotional drama, and a picturesque perspective on climate change in Tokyo.*/}
+            {/*                After selling millions of tickets in 2019, the film won Animaton of the Year award at the*/}
+            {/*                43rd annual Japan Academy Prize Ceremony as well as the Social Impact Award at the 23rd*/}
+            {/*                Japan Media Arts Festival.*/}
+            {/*                In other words, it&apos;s a must-see!</p>*/}
+            {/*        </Col>*/}
+            {/*        <Col md={4}>*/}
+            {/*            <MyImage src="images/wwy_poster.jpg" alt="Weathering With You poster"*/}
+            {/*                     className="border-stroke mw-100 mx-auto d-block"/>*/}
+            {/*        </Col>*/}
+            {/*    </Row>*/}
+            {/*</Container>*/}
         </>
     )
 }
