9fae5c7c963d142a17147dc002de33f9cec2194a
Kaitlyn <k6chan@ucsd.edu>
2023-01-18 17:55:36 -0800

    update rules

--- pages/rules.js
+++ pages/rules.js
@@ -18,21 +18,29 @@ export default function Rules() {
           <li><p>Anime &amp; Manga Enthusiasts and UC San Diego reserves the right to remove anyone not in compliance with the rules to their discretion.</p></li>
           <li><p><u>All university policies and regulations apply to attendees of Animefest.</u></p>
             <ul>
-              <li><p>Masking is highly encouraged.</p></li>
+              <li><p>Masking is <b>required at Animefest 2023.</b></p></li>
             </ul>
           </li>
           <li><p>Attendees must respect common sense rules for public behavior, personal interaction, common courtesy, and respect for private property. Harassing or offensive behavior will not be tolerated. If your behavior becomes problematic,
             security will have to intervene to dispute the issue.</p></li>
           <li><p>Attendees are prohibited from directly handling the console setups. Any attendee seen tampering or otherwise damaging Animefest property are liable for repairs and/or replacements.</p></li>
-          <li><p>Physical contact, photography, or recordings of other individuals is not allowed without their explicit permission.</p></li>
-          <li><p>No weapons or props designed or made with the ability to cause death or serious bodily injury to any person or property is permitted. Misuse of a prop will result in that prop being deemed unsafe.</p></li>
-          <li><p>Props resembling firearms are prohibited, including but not limited to: airsoft, replica, paintball, BB guns, and Nerf guns.</p></li>
-          <li><p>Please don&apos;t hesitate to call campus security, volunteers, or officers of Anime &amp; Manga Enthusiasts if you see anything suspicious or disorderly conduct.</p></li>
+          <li><p><b>Convention staff</b> may photograph and record video of the convention. By attending Animefest, you give consent to be included in these photographs and recordings by our staff.</p></li>
+          <li><p>Physical contact, photography, or recordings of other individuals by non-staff is not allowed without their explicit permission.</p></li>
+          <li><p>Please don&apos;t hesitate to call convention staff or campus security if you see anything suspicious or disorderly conduct.</p></li>
         </ol>
-        <p><u>COVID-19 Guidelines:</u> Festivalgoers are strongly advised to adhere to CDC guidelines which include:</p>
+        
+        <p><u>Cosplay & Prop Guidelines:</u></p>
+        <ol className="mb-5">
+          <li><p>No inappropriate (18+) cosplay is permitted.</p></li>
+          <li><p>No weapons or props designed or made with the ability to cause death or serious bodily injury to any person or property is permitted. Misuse of a prop will result in that prop being deemed unsafe.</p></li>
+          <li><p>Props resembling weapons are prohibited, including but not limited to bows, replica/toy swords, and firearms (airsoft, replica, paintball, BB guns, and Nerf guns).</p></li>
+          <li><p>All props will be checked and marked with tape by our convention staff at the registration table.</p></li>
+        </ol>
+        
+        <p><u>COVID-19 Addendum:</u> Attendees are strongly advised to adhere to CDC guidelines which include:</p>
         <ol className="mb-5">
           <li><p>Keeping fully vaccinated, and quarantine if coming in contact with an affected individual and getting tested.</p></li>
-          <li><p>Staying home if an attendee feels any sickness or symptoms of COVID-19 before the festival.</p></li>
+          <li><p>Staying home if an attendee feels any sickness or symptoms of COVID-19 before the convention.</p></li>
           <li><p>Maintaining 6 feet of social distancing whenever possible.</p></li>
           <li><p>Cover coughs and sneezes and wash hands.</p></li>
           <li><p>Enable contact tracing: On Android, contact tracing from Google can be enabled in your device&apos;s settings, or downloaded as an app for your state at the link <a href="https://support.google.com/android/answer/9888358?hl=en">here.</a> On iPhone, opt-in to Exposure Notifications in your device&apos;s settings.</p></li>
