7829852c54e375690b74493b4c000ba531802502
oweng00 <95007028+oweng00@users.noreply.github.com>
2023-02-02 23:57:24 -0800

    Added Board Game page, updated yugioh page w/ prizing and directions, added panels page

--- pages/boardgames.js
+++ pages/boardgames.js
@@ -1,6 +1,6 @@
-import {Container} from 'react-bootstrap' // add Col and Row when displaying image
+import {Col, Container, Row} from 'react-bootstrap' // add Col and Row when displaying image
 import BackgroundImage from "../components/index/backgroundimage"
-// import MyImage from "../components/myimage"
+import MyImage from "../components/myimage"
 
 
 export default function Boardgames() {
@@ -13,20 +13,58 @@ export default function Boardgames() {
             </Container>
 
             <Container fluid className="p-3 section">
+
+                
+
+            <Row className="mb-4">
+                    <Col xs={0} md={0} lg={0}/>
+                    <Col xs={0} md={0} lg={0}>
+                        <MyImage src="images/game_syougi.png" alt="Shogi Image"
+                        className="img-fluid" width="50%" height="50%"/>
+                        <MyImage src="images/toy_game_saikoro_10men.png" alt="Dice Image"
+                        className="img-fluid" width="50%" height="50%"/>
+                    </Col>
+                    <Col xs={0} md={0} lg={0}/>
+                </Row>
+
                 <h4 className="text-left mb-4 display-text">ABOUT</h4>
-                <p>Coming soon!</p> {/*add className="mb-4" when displaying image*/}
+                <p>The Board Games room will be at the Marshall College Room in Price Center.</p>
+                <p className="mb-4">Do you like good old-fashioned fun? Well in you're luck! Here in the
+                Board Games room, we got all kinda of 15-minute classics ranging from teamwork tabletops
+                like <i>Codenames!</i> to 1 hour high-stakes strategy games like <i>Catan</i>.</p>
 
-                {/*Rows and Cols is a grid system provided by React-Bootstrap. This allows things to be centered on a page.*/}
-                {/*The breakpoints for each display size should add up to 12. Learn more about breakpoints on getbootstrap.com*/}
+                <p>If any games catch your eye, stop by our room 11AM-3PM!</p>
 
-                {/*<Row className="mb-4">*/}
-                {/*    <Col xs={0} md={2} lg={4} />*/}
-                {/*    <Col xs={12} md={8} lg={4}>*/}
-                {/*        <MyImage src="images/board_game_club.jpg" alt="Board Game Club photo"*/}
-                {/*                 className="mw-100 border-stroke mx-auto d-block" />*/}
-                {/*    </Col>*/}
-                {/*    <Col xs={0} md={2} lg={4} />*/}
-                {/*</Row>*/}
+                
+
+                <h4 className="text-left mb-4 display-text">List of Games</h4>
+                <ul>
+                    <li>Crabs in a Bucket</li>
+                    <li>Sushi GO! Party</li>
+                    <li>Codenames! Pictures</li>
+                    <li>Trekking the World</li>
+                    <li>Meepleland</li>
+                    <li>Betrayal at the House on the Hill</li>
+                    <li>Catan</li>
+                    <li>Memoir '44</li>
+                    <li>Tiny Epic Western</li>
+                    <li>Tiny Epic Dinosaurs</li>
+                    <li>The Duke</li>
+                    </ul>
+                {/*add className="mb-4" when displaying image*/}
+
+                {/*Rows and Cols is a grid system provided by React-Bootstrap. This allows things to be centered on a page.
+                The breakpoints for each display size should add up to 12. Learn more about breakpoints on getbootstrap.com*/}
+
+                <Row className="mb-4">
+                    <Col xs={0} md={2} lg={4} />
+                    <Col xs={12} md={8} lg={4}>
+                        <MyImage src="images/board_game_club.jpg" alt="Board Game Club photo"
+                                 className="mw-100 border-stroke mx-auto d-block" />
+                    </Col>
+                    <Col xs={0} md={2} lg={4} />
+                </Row>
+                
             </Container>
         </>
     )
--- pages/yugioh.js
+++ pages/yugioh.js
@@ -1,12 +1,12 @@
 import {Col, Container, Row} from 'react-bootstrap'
 import MyImage from "../components/myimage"
 import BackgroundImage from "../components/index/backgroundimage"
-// import Prizes from "../data/prizes.json"
-// import PrizeImages from "../components/prizes/prizeimages"
+import Prizes from "../data/prizes.json"
+import PrizeImages from "../components/prizes/prizeimages"
 
 
 export default function Yugioh() {
-    // const prizesInPool = Prizes["prizes"].filter((prize) => prize["pool"] === "Smash Ultimate").map((prize) => prize["image"])
+    const prizesInPool = Prizes["prizes"].filter((prize) => prize["pool"] === "YGO").map((prize) => prize["image"])
     return (
         <>
             <Container fluid className="p-0 text-center">
@@ -27,8 +27,10 @@ export default function Yugioh() {
                     <Col xs={0} md={2} lg={3}/>
                 </Row>
 
-                <p>Gamma Gamma Drive Yu-gi-oh will be hosting a tournament for Yu-gi-oh TCG! The tournament will be <b>free
-                    to enter</b>, and open to all <b>UCSD Students and Alumni</b>.</p>
+                <p className="mb-0">Gamma Gamma Drive Yu-gi-oh will be hosting a tournament for Yu-gi-oh TCG! The tournament will be <b>free
+                    to enter</b>, and open to all <b>UCSD Students and Alumni</b>. Feel free to join our Discord server for more information!</p>
+                <p><a 
+                    href="https://discord.gg/e2vBZA7v">https://discord.gg/e2vBZA7v</a></p>
                 <h4 className="text-left title-text">Register:</h4>
                 <p>You can register for the event on site during Animefest.</p>
                 <h4 className="text-left title-text">Cost and Prizing:</h4>
@@ -46,16 +48,19 @@ export default function Yugioh() {
                     <p>Doors will open at <b>1 PM</b>, and the bracket will start at <b>1 PM.</b> Free play will begin after
                     the tournament ends, around <b>5 PM.</b></p>
                 <h4 className="text-left title-text">Location:</h4>
-                <p>We’ll be in the <b>Green Table Room</b>, which is inside Sun God Lounge.</p>
+                <p className="mb-0">We’ll be in the <b>Green Table Room</b>, which is inside Sun God Lounge. You can find
+                directions to the Sun God Lounge on the Triton Smash webpage.</p> 
+                <p><a 
+                    href="https://animefest.ucsd.moe/gaming">Triton Smash</a></p>
             </Container>
 
             {/*when uncommenting the below section, plus any other line with the word prizes in it, a prize*/}
             {/*section will be created displaying all the prize images for Triton Smash*/}
 
-            {/*<Container fluid className="p-3 mt-4 section">*/}
-            {/*    <h4 className="text-left mb-4 display-text">PRIZING</h4>*/}
-            {/*    <PrizeImages prizes={prizesInPool}/>*/}
-            {/*</Container>*/}
+            <Container fluid className="p-3 mt-4 section">
+                <h4 className="text-left mb-4 display-text">PRIZING</h4>
+                <PrizeImages prizes={prizesInPool}/>
+            </Container>
         </>
     )
 }
