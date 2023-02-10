977c150bbaf0cc3953425aea3fc174a241fbe8e6
Kaitlyn <k6chan@ucsd.edu>
2023-02-10 00:09:21 -0800

    reveal maps and schedule

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ export default function Home() {
                 <p>All rooms are located on the second floor with the exception of the Theater, which
                     is on the first
                     floor and below the Bear Room.</p>
-                <p className="mb-4"><i>Schedule coming soon!</i></p>
+                {/*Example of embedding a Google Sheet (2022) in the below div, using custom sheet classes*/}
                 {/*<div className="text-center mb-5">*/}
                 {/*    <iframe className="sheet-schedule sheet-constraint w-75 d-block mb-1 mx-auto"*/}
                 {/*            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSh2gBYXSEV57p05RAVENMwqDHmJ5GFHTjgb0M9LOTAYrBbLrrwA5fMECz7R3F2qfpnRElr854rGZOG/pubhtml?widget=true&amp;headers=false"/>*/}
@@ -74,11 +74,33 @@ export default function Home() {
                 {/*        trouble viewing? Click here!</a></p>*/}
                 {/*</div>*/}
                 <div className="text-center">
-                    <a href="images/map.jpg">
-                        <MyImage src="images/map.jpg" className="sheet-constraint border-stroke mw-100 mb-1"
-                            alt="Price Center map"></MyImage>
-                    </a>
-                    <p className="caption-text">Click on the image to open.</p>
+                    <Row className="mb-4">
+                        <Col xs={0} md={2} />
+                        <Col xs={12} md={8}>
+                            <a href="images/schedule.png">
+                                <MyImage src="images/schedule.png" className="border-stroke mw-100"
+                                         alt="Schedule"></MyImage>
+                            </a>
+                        </Col>
+                        <Col xs={0} md={2} />
+                    </Row>
+                    <Row className="mb-1 g-3">
+                        <Col xs={0} md={0} lg={2}/>
+                        <Col xs={12} md={6} lg={4}>
+                            <a href="images/map_2nd_floor.jpg">
+                                <MyImage src="images/map_2nd_floor.jpg" className="border-stroke mw-100"
+                                         alt="Price Center 2nd Floor map"></MyImage>
+                            </a>
+                        </Col>
+                        <Col xs={12} md={6} lg={4}>
+                            <a href="images/map_1st_floor.jpg">
+                                <MyImage src="images/map_1st_floor.jpg" className="border-stroke mw-100"
+                                         alt="Price Center 1st Floor map"></MyImage>
+                            </a>
+                        </Col>
+                        <Col xs={0} md={0} lg={2}/>
+                    </Row>
+                    <p className="caption-text">Click on the images to open.</p>
                 </div>
             </Container>
 
