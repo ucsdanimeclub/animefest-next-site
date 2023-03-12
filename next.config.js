a701a67b34164a43fd6d4ac89745a461048bd0cb
Kaitlyn <k6chan@ucsd.edu>
2023-03-12 13:25:50 -0700

    basepath advice

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 const useSubDirectory = process.env.USE_SUB_DIRECTORY === 'true'
 
+// To host on ucsd.moe and animefest.ucsd.edu:
+// assetPrefix: useSubDirectory ? '/animefest-next-site/' : '',
+// basePath: useSubDirectory ? '/animefest-next-site' : ''
+
+// To host on animeclub.ucsd.edu:
+// assetPrefix: '/~animeclub/animefest/',
+// basePath: '/~animeclub/animefest'
+
 module.exports = {
   reactStrictMode: true,
   assetPrefix: useSubDirectory ? '/animefest-next-site/' : '',
