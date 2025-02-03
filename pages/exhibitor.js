b7cb4aeff7b55d521333a3057065660dc5d8d719
TurtleLions <ryanestanislao@gmail.com>
2025-02-03 12:51:13 -0800

    artists complete

--- pages/exhibitor.js
+++ pages/exhibitor.js
@@ -17,9 +17,36 @@ export default function Exhibitor() {
 
             <Container fluid className="p-3 section">
                 <h4 className="text-left mb-4 display-text">ABOUT</h4>
-                <p className="mb-4">We are currently updating our Artist Alley and Vendor list!
-                    Check back soon!
-                </p>
+                <p className="mb-4">The Exhibitor Hall is home to our Artist Alley and Vendor space. It is located
+                    throughout the West
+                    Ballroom.</p>
+                <h4 className="text-left title-text mb-4">Artists</h4>
+                <Row>
+                    <Col xs={0} md={1} lg={2}>
+                    </Col>
+                    <Col xs={12} md={10} lg={8}>
+                        <ExhibitorTable data={Artists} className="mb-4"/>
+                    </Col>
+                    <Col xs={0} md={1} lg={2}>
+                    </Col>
+                </Row>
+                <h4 className="text-left title-text mb-4">Vendors</h4>
+                <Row>
+                    <Col xs={0} md={1} lg={2}>
+                    </Col>
+                    <Col xs={12} md={10} lg={8}>
+                        <ExhibitorTable data={Vendors} className="mb-4"/>
+                    </Col>
+                    <Col xs={0} md={1} lg={2}>
+                    </Col>
+                </Row>
+                <h4 className="text-left title-text mb-4">West Ballroom Map</h4>
+                <div className="text-center">
+                    <a href="images/west_ballroom_exhibitors_2024.png"><MyImage src="images/west_ballroom_exhibitors_2024.png"
+                                                                           alt="West Ballroom map"
+                                                                           className="mw-100 mb-1 border-stroke"/></a>
+                    <p className="caption-text mb-0">Click on the image to open.</p>
+                </div>
             </Container>
         </>
     )
